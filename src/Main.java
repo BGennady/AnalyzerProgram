8776887c22267382f62dbb27190d01868e26be8b
BGennady <bg.korolew@gmail.com>
2024-10-22 23:19:06 +0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,85 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+public class Main {
+    public static final int COUNT_TEXTS = 10000; // количество текстов
+    public static final int LENGT_TEXTS = 100000; // длина каждого текста
+    public static final int QUEUE_CAPACITY = 100; // // размер блокирующих очередей
+    public static long countA = 0;
+    public static long countB = 0;
+    public static long countC = 0;
+
+    // блокирующие очереди для каждого символа
+    public static BlockingQueue<String> queueA = new ArrayBlockingQueue<>(QUEUE_CAPACITY);
+    public static BlockingQueue<String> queueB = new ArrayBlockingQueue<>(QUEUE_CAPACITY);
+    public static BlockingQueue<String> queueC = new ArrayBlockingQueue<>(QUEUE_CAPACITY);
+
+    public static void main(String[] args) throws InterruptedException {
+
+        // поток генерации текстов
+        Thread generate = new Thread(() -> {
+            for (int i = 0; i < COUNT_TEXTS; i++) {
+                String texts = GenerateText.generateText("abc", LENGT_TEXTS);
+                try {
+                    queueA.put(texts);
+                    queueB.put(texts);
+                    queueC.put(texts);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        });
+
+        Thread analyzerA = new Thread(() -> {
+            char target = 'a';
+            try {
+                for (int i = 0; i < COUNT_TEXTS; i++) {
+                    String text = queueA.take(); // извлекаем текст из очереди
+                    countA += text.chars()       // cчитаем все вхождения 'a' за один проход
+                            .filter(ch -> ch == target)
+                            .count();
+                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        Thread analyzerB = new Thread(() -> {
+            char target = 'b';
+            try {
+                for (int i = 0; i < COUNT_TEXTS; i++) {
+                    String text = queueB.take(); // извлекаем текст из очереди
+                    countB += text.chars()       // cчитаем все вхождения 'b' за один проход
+                            .filter(ch -> ch == target)
+                            .count();
+                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        Thread analyzerC = new Thread(() -> {
+            char target = 'c';
+            try {
+                for (int i = 0; i < COUNT_TEXTS; i++) {
+                    String text = queueC.take();  // извлекаем текст из очереди
+                    countC += text.chars()        // cчитаем все вхождения 'c' за один проход
+                            .filter(ch -> ch == target)
+                            .count();
+                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        generate.start();
+        analyzerA.start();
+        analyzerB.start();
+        analyzerC.start();
+
+        generate.join();
+        analyzerA.join();
+        analyzerB.join();
+        analyzerC.join();
+
+        System.out.printf("Общее количество символов: \n a = %d \n b = %d \n c = %d", countA, countB, countC);
     }
 }
